bf022ded78829b2a181d40bbabeafccc594243cf
Alex28 Gol28 spacedR <sasha2015to@gmail.com>
2019-09-18 23:15:09 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $('#inputName').on('keyup', function (event) {
     $('#textHolder').text($(this).val())
-    if (event.which == 27) {
+    if (event.which == 65) {
         // alert('It was ESCAPE key! ')
         $('#textHolder').text('')
     }
+    console.log(event.which)
 })
